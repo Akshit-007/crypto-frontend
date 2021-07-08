d6d87290750d53a5c459cba972841a3034fb926e
sahildabhi0101 <63159436+sahildabhi0101@users.noreply.github.com>
2021-07-08 16:20:15 +0530

    3

---
+++ src/user/data.js
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+const API = "https://api.nomics.com/v1/currencies/ticker?key=cbd1795669ba0116a656db6c0fcfbd1f55de6357&ids=BTC,ETH,XRP,SAFEMOON,BTT,DOGE&interval=1d,30d&convert=USD&per-page=100&page=1"
+
+const fetchCrypto = () => {
+
+    return fetch(API, {
+        method: "GET"
+    })
+    .then((response) => response.json())
+    .catch(err => { console.log(err)
+    })
+}
+
+export default fetchCrypto;
--- src/user/home.js
+++ src/user/home.js
@@ -1,32 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../css/style.css'
 import Nav from "./nav"
-
+import fetchCrypto from './data';
 const Home = () => {
+    const [cryptos, setCryptos] = useState([])
+    const [reload, setReload] = useState(false)
+
+    setInterval(() => { setReload(!reload) }, 10000);
+
+    const fetch = () => {
+        fetchCrypto()
+            .then(data => {
+                setCryptos(data)
+            })
+            .catch(err => console.log(err))
+    }
+
+    useEffect(() => {
+        fetch()
+    }, [reload])
     return (
         <>
             <div className="home">
                 <Nav />
                 <div className="main-container">
                     <div className="firstrow">
-                        <a>#</a>
-                        <a>NAME</a>
-                        <a>PRICE</a>
-                        <a>1D CHANGE</a>
+                        <a href="/">#</a>
+                        <a href='/'>NAME</a>
+                        <a href='/'>PRICE</a>
+                        <a href='/'>1D CHANGE</a>
                     </div>
+
                     <div className="all">
-                        <div className="allrow">
-                            <li>#</li>
-                            <li>NAME</li>
-                            <li>PRICE</li>
-                            <li>1D CHANGE</li>
-                        </div>
-                        <div className="allrow">
-                            <li>#</li>
-                            <li>NAME</li>
-                            <li>PRICE</li>
-                            <li>1D CHANGE</li>
-                        </div>
+                        {
+                            cryptos.map((value, key) => (
+                                <div className="allrow">
+                                    <li>{value.currency}</li>
+                                    <li>{value.name}</li>
+                                    <li>{value.price < 1 ? (value.price) : (Math.trunc(value.price))}</li>
+                                    <li>{value.rank}</li>
+
+                                </div>
+                            ))
+                        }
                     </div>
                 </div>
             </div>
