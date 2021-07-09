31e90fb680154a303d5056326fed2fcd45c46226
Akshit <akshittrivedi1005@gmail.com>
2021-07-09 23:03:46 +0530

    first

--- src/user/data.js
+++ src/user/data.js
@@ -1,15 +1,17 @@
 // import React from 'react'
 
-const API = "https://api.nomics.com/v1/currencies/ticker?key=cbd1795669ba0116a656db6c0fcfbd1f55de6357&ids=BTC,ETH,XRP,SAFEMOON,BTT,DOGE,THETA&interval=1d,30d&convert=USD&per-page=100&page=1"
 
-const fetchCrypto = () => {
+const fetchCrypto = (currency) => {
+
+    const API = `https://api.nomics.com/v1/currencies/ticker?key=cbd1795669ba0116a656db6c0fcfbd1f55de6357&interval=1d,30d&convert=${currency}&per-page=100&page=1`
 
     return fetch(API, {
         method: "GET"
     })
-    .then((response) => response.json())
-    .catch(err => { console.log(err)
-    })
+        .then((response) => response.json())
+        .catch(err => {
+            console.log(err)
+        })
 }
 
 export default fetchCrypto;
--- src/user/home.js
+++ src/user/home.js
@@ -2,56 +2,149 @@ import React, { useState, useEffect } from 'react'
 import '../css/style.css'
 import Nav from "./nav"
 import fetchCrypto from './data';
+import { makeStyles } from '@material-ui/core/styles';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
+import { Paper } from '@material-ui/core';
+import { ArrowUpward } from '@material-ui/icons';
+import { ArrowDownward } from '@material-ui/icons';
+
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+    },
+});
+
+
+
 const Home = () => {
     const [cryptos, setCryptos] = useState([])
     const [reload, setReload] = useState(false)
+    const [currency, setCurrency] = useState('USD')
+    const [time, setTime] = useState('1d')
 
     setInterval(() => { setReload(!reload) }, 10000);
 
     const fetch = () => {
-        fetchCrypto()
+        fetchCrypto(currency)
             .then(data => { setCryptos(data) })
             .catch(err => console.log(err))
+
+
     }
 
-    useEffect(() => { fetch() }, [reload])
-    console.log(cryptos);
+    useEffect(() => { fetch() }, [reload], [currency])
+    // console.log(cryptos);
+
+    function handleChangecurrency(event) {
+        setCurrency(event.target.value);
+    }
+
+    function handleTimeChange(event) {
+        event.preventDefault();
+        setTime(event.target.value);
+        // console.log(time);
+
+        let a = document.getElementById('day');
+        let b = document.getElementById('month');
+
+        if (event.target.id === 'day') {
+            a.style.backgroundColor = "green";
+            a.style.color = "white";
+            b.style.backgroundColor = "white";
+            b.style.color = "black";
+        }
+        else {
+            a.style.backgroundColor = "white";
+            a.style.color = "black";
+            b.style.backgroundColor = "green";
+            b.style.color = "white";
+        }
+
+    }
+
+
+
+
+    const classes = useStyles();
     return (
         <>
             <div className="home">
                 <Nav />
-                <div className="main-container">
-                    <div className="firstrow">
-                        <a href="/">#RANK</a>
-                        <a href="/">TAG</a>
-                        <a href='/'>NAME</a>
-                        <a href='/'>PRICE</a>
-                        <a href="/">#MARKET CAP</a>
-                        <a href='/'>1D CHANGE</a>
-                    </div>
+                <br />
+                <br />
 
-                    <div className="all">
-                        {
-                            cryptos.map((value, key) => (
-                                <div className="allrow">
-                                    <li>{value.rank}</li>
-                                    <li>{value.currency}</li>
-                                    {
-                                        <li>
-                                            <img className="imglogo" src={value.logo_url} alt="" />
-                                            {value.name}</li>
-                                    }
-                                    <li>${value.price < 1 ? parseFloat(value.price).toFixed(5) : (parseFloat(value.price).toFixed(2))}</li>
-                                    <li>{parseFloat(value.market_cap / 1000000000).toFixed(2)} B</li>
-                                    <li className={value["1d"].price_change_pct * 100 < 0 ? 'red' : 'green'} >{parseFloat(value["1d"].price_change_pct * 100).toFixed(2)}%</li>
+                <div className="chooseOption">
+
+                    <form >
+                        <button value="1d" className="timebtn" id="day" onClick={handleTimeChange}>1 day</button>&nbsp;
+                        <button value="30d" className="timebtn" id="month" onClick={handleTimeChange}>1 month</button>
+
+                        &nbsp;  &nbsp;  &nbsp;  &nbsp;
+                        <label for="currency" className="currencyLabel">Choose a currency:</label>
+                        <select name="currency" id="currency" onChange={handleChangecurrency}>
+                            <option value="USD">USD</option>
+                            <option value="INR">INR</option>
+                            <option value="EUR">EUR</option>
+
+                        </select>
 
 
-                                </div>
-                            ))
-                        }
-                    </div>
+                    </form>
+
                 </div>
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+
+                <div className="crypto">
+
+                    <TableContainer component={Paper}>
+                        <Table className={classes.table} aria-label="simple table">
+                            <TableHead>
+                                <TableRow className="tableHead">
+                                    <TableCell><span className="headTitle">#</span></TableCell>
+
+                                    <TableCell align="left"><span className="headTitle">NAME</span></TableCell>
+                                    <TableCell align="left"><span className="headTitle">PRICE&nbsp;  ({currency})</span></TableCell>
+                                    <TableCell align="left"><span className="headTitle">MARKET CAP</span></TableCell>
+                                    <TableCell align="left"><span className="headTitle">CHANGE&nbsp;({time})</span></TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {cryptos.length === 0 ? <div><h3>Fetching Data</h3></div> : <>
+                                    {cryptos.map((row) => (
+                                        <TableRow key={row.rank} className="tableRow">
+                                            <TableCell component="th" scope="row">
+                                                <span className="cryptoData"> {row.rank}</span>
+                                            </TableCell>
+                                            <TableCell align="left">{<img src={row.logo_url} className="coinLogo"></img>}&nbsp;&nbsp;&nbsp;&nbsp;<span className="cryptoData">{row.name}</span>&nbsp;&nbsp; {row.currency}</TableCell>
+
+                                            <TableCell align="left"> <span className="cryptoData"> {currency === 'USD' ? <>$</> : <>{currency === 'INR' ? <>₹</> : <>ē</>}</>}&nbsp;&nbsp;{row.price < 1 ? parseFloat(row.price).toFixed(5) : (parseFloat(row.price).toFixed(2))}</span>
+                                            </TableCell>
+                                            <TableCell align="left"> <span className="cryptoData"> {parseFloat(row.market_cap / 1000000000).toFixed(2)} B</span>
+                                            </TableCell>
+                                            <TableCell align="left"> <span className="cryptoData">{time === '1d' ? <>{parseFloat(row["1d"].price_change_pct) > 0 ? <><span className="increase"><ArrowUpward />&nbsp;{row["1d"].price_change_pct}</span></> : <><span className="decrease"><ArrowDownward />&nbsp;{row["1d"].price_change_pct}</span></>}</> : <>{parseFloat(row["30d"].price_change_pct) > 0 ? <><span className="increase"><ArrowUpward />&nbsp;{row["30d"].price_change_pct}</span></> : <><span className="decrease"><ArrowDownward />&nbsp;{row["30d"].price_change_pct}</span></>}</>} %</span>
+                                            </TableCell>
+                                        </TableRow>
+
+                                    ))}
+                                </>}
+
+
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </div>
+
             </div>
+
+
+            {/* // New Table */}
+
+
         </>
     );
 };
