19861653b3780826e44ef8b48413d52000c39fde
sahildabhi0101 <63159436+sahildabhi0101@users.noreply.github.com>
2021-07-16 09:48:42 +0530

    sa8

--- src/user/nav.js
+++ src/user/nav.js
@@ -1,26 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../css/style.css'
+import foodlogohome from './food-logo.PNG'
 import { isauthenticated, signout } from '../auth'
 const Nav = () => {
     return (
         <>
             <nav className="nav" >
                 <div className="nav-container">
-                    <li><Link to="/">Home</Link></li>
-                    {!isauthenticated() && (
-                        <>
-                            <li><Link to='/signup'>signup</Link></li>
-                            <li><Link to="/signin">Login</Link></li>
-                        </>
-                    )}
-                    {isauthenticated() && (
-                        <>
-                            <li><Link to={`/user/${isauthenticated().user._id}`}>{` ${isauthenticated().user.name}`}</Link></li>
-                            <li><Link to={`/user/favourite/${isauthenticated().user._id}`} >Favourite</Link></li>
-                            <li><a href="?" onClick={() => signout(() => this.props.history.push('/'))}>signout</a></li>
-                        </>
-                    )}
+                    <div className="logomain1">
+                        <img src={foodlogohome} alt="food-logo" className="logomain" />
+                    </div>
+                    <div className="rightside">
+                        <li><Link to="/">Home</Link></li>
+                        {!isauthenticated() && (
+                            <>
+                                <li><Link to='/signup'>signup</Link></li>
+                                <li><Link to="/signin">Login</Link></li>
+                            </>
+                        )}
+                        {isauthenticated() && (
+                            <>
+                                <li><Link to={`/user/${isauthenticated().user._id}`}>{` ${isauthenticated().user.name}`}</Link></li>
+                                <li><Link to={`/user/favourite/${isauthenticated().user._id}`} >Favourite</Link></li>
+                                <li><a href="?" onClick={() => signout(() => this.props.history.push('/'))}>signout</a></li>
+                            </>
+                        )}
+                    </div>
                 </div>
             </nav>
         </>
