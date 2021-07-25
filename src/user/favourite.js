c3f0558d09f41a2560e83c1723dd2715257ee3ae
sahildabhi0101 <63159436+sahildabhi0101@users.noreply.github.com>
2021-07-25 21:02:43 +0530

    sa20

--- src/user/favourite.js
+++ src/user/favourite.js
@@ -18,7 +18,7 @@ const Favourite = () => {
     const [loading, setLoading] = useState(false)
     const [noFav, setNoFav] = useState(false)
 
-    setInterval(() => { setReload(!reload) }, 10000);
+    setInterval(() => { setReload(!reload) }, 30000);
 
     function removefromfav(currency) {
         setLoading(true);
@@ -27,7 +27,7 @@ const Favourite = () => {
         }
         toast.error(` ${currency} remove from favourite `, {
             position: "top-right",
-            autoClose: 8000,
+            autoClose: 3000,
             hideProgressBar: false,
             closeOnClick: true,
             pauseOnHover: true,
